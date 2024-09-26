3f11829b5fa6f962a140fc43c3c5dc10ff970861
Peter Shen <peterzhuo.shen@gmail.com>
2024-09-26 13:05:15 -0700

    Deploy website - based on b0faa2516554b33d1f9ae8b0c713e57d008e4245

---
+++ assets/js/2cd5636e.d9a064c9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[501],{8626:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/next-motion-ai-pages/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/814f3328.b922961f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/next-motion-ai-pages/blog/welcome","unlisted":false}]}')}}]);
